d53e6c242938300649bdcb403a782706ce3fb254
AleksAngelovski <aleksandar.angelovski1993@gmail.com>
2022-06-27 11:38:48 +0200

    First commit

---
+++ Console1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string userValue = Console.ReadLine();
+if(userValue == "1")
+{
+    string message = "typed 1 ";
+    Console.WriteLine(message);
+    
+}
